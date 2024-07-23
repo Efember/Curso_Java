e91059a233cdf6536f042d6148be17ee6bf95903
Efember <158684809+Efember@users.noreply.github.com>
2024-07-22 21:46:23 -0600

    Update Suma.java

--- Suma.java
+++ Suma.java
@@ -1,6 +1,7 @@
+/* ----------------------Division---------------------------------*/
 import java.util.Scanner;
 
-public class Suma {
+public class Division {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
@@ -19,6 +20,68 @@ public class Suma {
         } else {
             System.out.println("No se puede dividir entre 0.");
         }
+
+    }
+}
+
+
+/*-----------------------Suma solo enteros---------------------------*/
+
+import java.util.Scanner;
+
+public class Suma {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Ingresa el primer numero");
+        int numero1 = entrada.nextInt();
+
+        System.out.println("Ingrese el segundo nombre");
+        int numero2 = entrada.nextInt();
+
+        int suma = numero1 + numero2;
+        System.out.println("La suma es: " + suma);
+
+    }
+}
+
+
+/*-----------------------resta -----------------------------*/
+
+import java.util.Scanner;
+
+public class Resta {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Ingresa el primer numero");
+        int numero1 = entrada.nextInt();
+
+        System.out.println("Ingrese el segundo nombre");
+        int numero2 = entrada.nextInt();
+
+        int suma = numero1 - numero2;
+        System.out.println("La suma es: " + suma);
+
+    }
+}
+
+*/------------------Multiplicacion-------------------------*/
+
+import java.util.Scanner;
+
+public class Multiplicacion {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Ingresa el primer numero");
+        float numero1 = entrada.nextInt();
+
+        System.out.println("Ingrese el segundo nombre");
+        float numero2 = entrada.nextInt();
+
+        float mult = numero1 * numero2;
+        System.out.println("La Multiplicacion es:" + mult);
 
     }
 }
